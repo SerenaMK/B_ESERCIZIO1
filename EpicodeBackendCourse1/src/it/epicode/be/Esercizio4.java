45e58e8e39fd153ed6a2833c4eba3694cb7289af
Serena <114289210+SerenaMK@users.noreply.github.com>
2023-01-10 18:04:29 +0100

    Esercizio completato

--- EpicodeBackendCourse1/src/it/epicode/be/Esercizio4.java
+++ EpicodeBackendCourse1/src/it/epicode/be/Esercizio4.java
@@ -4,7 +4,20 @@ public class Esercizio4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(perimetroR(10.60, 5.30));
+        System.out.println(pariDispari(55));
 	}
+	
+	static double perimetroR(double a, double b) {
+        return (a + b) * 2;
+    }
+
+    static int pariDispari(int a) {
+        return a % 2;
+    }
+
+    static double perimetroT() {
+        return 5.6;
+    }
 
 }
